d0a9645bc08803fde207c5f19264c48a6539cad7
Vladis Markin <vladism@wix.com>
2022-05-23 17:31:26 +0300

    all-in-one

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World</h1>,document.getElementById('root'));
+// Not a component, but a function that returns a string.
+// Component should be a function that returns a JSX Element (React element under the hood).
+
+// const BadComponent = () => {
+//     return 'Hello World';
+// }
+
+// A valid component using JSX.
+
+// const Component = () => {
+//     return (
+//         <main>
+//             <h1 id="title">Who is the king of Ktatala?</h1>
+//         </main>
+//     );
+// };
+
+// A valid Component using React Top Level API
+
+// const Component = () => {
+//     return React.createElement(
+//         'main',
+//         {},
+//         React.createElement('h1', { id: 'title' }, 'Who is the king of Ktatala?'),
+//     );
+// };
+
+// A valid React Element
+
+const reactElement = {
+    $$typeof: Symbol.for('react.element'),
+    type: 'main',
+    key: null,
+    ref: null,
+    props: {
+        children: {
+            $$typeof: Symbol.for('react.element'),
+            type: 'h1',
+            key: null,
+            ref: null,
+            props:{
+                id: 'title',
+                children: 'Who is the king of Ktatala?'
+            },
+        }
+    },
+}
+
+ReactDOM.render(reactElement,document.getElementById('root'));
