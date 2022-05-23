dec2800a424bc02af89ebade1e1da01435f2dbe2
Vladis Markin <vladism@wix.com>
2022-05-23 17:13:25 +0300

    A valid React Element

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World</h1>,document.getElementById('root'));
+// A valid React Element
+
+const reactElement = {
+    $$typeof: Symbol.for('react.element'),
+    type: 'main',
+    key: null,
+    ref: null,
+    props: {
+        children: {
+            $$typeof: Symbol.for('react.element'),
+            type: 'h1',
+            key: null,
+            ref: null,
+            props:{
+                id: 'title',
+                children: 'Who is the king of Ktatala?'
+            },
+        }
+    },
+}
+
+ReactDOM.render(reactElement,document.getElementById('root'));
