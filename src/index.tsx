4a4e28232e17b694d2bcc2522a27f54f5f8f5ab3
Vladis Markin <vladism@wix.com>
2022-05-23 16:03:23 +0300

    A valid Component using React Top Level API

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World</h1>,document.getElementById('root'));
+// A valid Component using React Top Level API
+
+const Component = () => {
+    return React.createElement(
+        'main',
+        {},
+        React.createElement('h1', { id: 'title' }, 'Who is the king of Ktatala?'),
+    );
+};
+
+ReactDOM.render(<Component/>,document.getElementById('root'));
