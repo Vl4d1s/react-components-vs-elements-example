d8206733e5bc40b2e10796553c2cb1435d28f80f
Vladis Markin <42187212+Vl4d1s@users.noreply.github.com>
2022-05-23 13:59:20 +0300

    A valid component using JSX (#1)

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World</h1>,document.getElementById('root'));
+// Not a component, but a function that returns a string.
+// Component should be a function that returns a JSX Element (React element under the hood).
+
+// const BadComponent = () => {
+//     return 'Hello World';
+// }
+
+// A valid component using JSX.
+
+const Component = () => {
+    return (
+        <main>
+            <h1 id="title">Who is the king of Ktatala?</h1>
+        </main>
+    );
+};
+
+
+ReactDOM.render(<Component/>,document.getElementById('root'));
